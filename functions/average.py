c0dee2348616735330623a261033f2e3de66d6ec
Lennart Edler <lennart.edler@owita.com>
2024-06-12 12:54:06 +0200

    round average output

--- functions/average.py
+++ functions/average.py
@@ -3,8 +3,9 @@ def getListAverage(lst):
     for x in lst:
         sum += x
     average = sum / len(lst)
-    return average
+    return round(average, 2)
 
 if __name__ == "__main__":
     zahlen = [4, 23, 8, 13, 7, 9, 42, 11, 6, 80, 3]
     print("Number average of list:", getListAverage(zahlen))
+
