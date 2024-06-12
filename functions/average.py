c5f31907b7b02878cd8fe74bdceff8a4a6aeedd7
Lennart Edler <lennart.edler@owita.com>
2024-06-12 12:52:59 +0200

    add average function

--- functions/average.py
+++ functions/average.py
@@ -1,5 +1,10 @@
-def getListAverage():
-    print("Number average of list")
+def getListAverage(lst):
+    sum = 0
+    for x in lst:
+        sum += x
+    average = sum / len(lst)
+    return average
 
-if __name__=="__main__":
-    getListAverage()
+if __name__ == "__main__":
+    zahlen = [4, 23, 8, 13, 7, 9, 42, 11, 6, 80, 3]
+    print("Number average of list:", getListAverage(zahlen))
