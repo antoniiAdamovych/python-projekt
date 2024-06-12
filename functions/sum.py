798f99a6a61e5675d0ad0606a9de96113cbf0d08
Lennart Edler <lennart.edler@owita.com>
2024-06-12 12:58:50 +0200

    add sum function

--- functions/sum.py
+++ functions/sum.py
@@ -1,5 +1,9 @@
-def getNumbersSum():
-    print("Sum of all numbers in List")
+def getNumbersSum(lst):
+    sum = 0
+    for x in lst:
+        sum += x
+    return sum
 
-if __name__=="__main__":
-    getNumbersSum()
+if __name__ == "__main__":
+    zahlen = [4, 23, 8, 13, 7, 9, 42, 11, 6, 80, 3]
+    print("Sum of numbers in list:", getNumbersSum(zahlen))
