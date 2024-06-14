ac147eb43f5c4d8aed7f31dca3183aec221f18f8
Lennart Edler <lennart.edler@owita.com>
2024-06-14 12:48:06 +0200

    Menu in main()

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 def main():
-    print("Our Python Project")
+    print("Willkommen! Bitte wählen Sie eine Figur für die Flächenberechnung:")
+    print("1. Kleinster Wert")
+    print("2. Größter Wert")
+    print("3. Summe aller Zahlen")
+    print("4. Durchschnitt berechnen")
+    print("5. Liste Sortieren")
+    print("6. Median ausgeben")
+    
+    auswahl = int(input("Geben Sie die Nummer der gewünschten Figur ein: "))
+    
+    if auswahl == 1:
+        #getSmallestNumber()
+        pass
+    elif auswahl == 2:
+        #getBiggestNumber
+        pass
+    elif auswahl == 3:
+        #getNumbersSum()
+        pass
+    elif auswahl == 4:
+        #getListAverage
+        pass
+    elif auswahl == 5:
+        #getSortList()
+        pass
+    elif auswahl == 6:
+        #getMedian()s
+        pass
+    else:
+        print("Ungültige Auswahl!")
 
 if __name__=="__main__":
     main()
