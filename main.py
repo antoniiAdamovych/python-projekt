b4774e64af5f87709b82422e0b08907fc182e52f
RH-03 <RaHess@schueco.com>
2024-06-19 12:28:14 +0200

    merge conflict

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from functions.median import getMedian
+<<<<<<< HEAD
 from functions.sortList import quickSort
 
+=======
+from functions.smallest import getSmallestNumber
+from functions.biggest import getBiggestNumber
+from functions.average import getListAverage
+from functions.sum import getNumbersSum
+from functions.sortList import getSortList
+>>>>>>> d9fd65091b111e57acec478d646a92a7c3613dcc
 class bcolors:
     OKGREEN = '\033[92m'
     ENDC = '\033[0m'
@@ -36,19 +44,23 @@ def main():
     array = RequestAnArray()
     
     if auswahl == 1:
-        #getSmallestNumber()
+        getSmallestNumber()
         pass
     elif auswahl == 2:
-        #getBiggestNumber
+        getBiggestNumber
         pass
     elif auswahl == 3:
-        #getNumbersSum()
+        getNumbersSum()
         pass
     elif auswahl == 4:
-        #getListAverage
+        getListAverage
         pass
     elif auswahl == 5:
+<<<<<<< HEAD
         quickSort(array, 0, len(array)-1)
+=======
+        getSortList()
+>>>>>>> d9fd65091b111e57acec478d646a92a7c3613dcc
         pass
     elif auswahl == 6:
         getMedian(array)
